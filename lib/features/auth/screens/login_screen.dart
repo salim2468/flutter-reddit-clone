a62f3b295ba1a5d0e822d1f8585e9a2ad5c917c7
salim2468 <salimmaharjan2468@gmail.com>
2023-06-17 14:57:59 +0545

    appbar title center issue fixed on android

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -9,6 +9,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: Image.asset(
             Constant.logoPath,
             height: 40,
